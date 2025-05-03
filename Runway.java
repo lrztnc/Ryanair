f3007e7767b8ddbd010afb8947632b05559a0305
lrztnc <lorenzo.tincani@icloud.com>
2025-05-03 17:26:37 +0200

    Add Runway class to manage runway operations and plane assignments

---
+++ Runway.java
@@ -0,0 +1,47 @@
+public class Runway {
+
+    private int id;                
+    private Plane currentPlane; 
+
+    public Runway(int id) {
+        this.id = id;
+        this.currentPlane = null; 
+    }
+
+    public int getId() {
+        return id;
+    }
+
+   
+    public boolean isAvailable() {
+        return currentPlane == null;
+    }
+
+    
+    public boolean assignPlane(Plane airplane) {
+        if (isAvailable()) {
+            this.currentPlane = airplane;
+            return true;
+        }
+        return false; 
+    }
+
+   
+    public void clearRunway() {
+        this.currentPlane = null;
+    }
+
+   
+    public Plane getCurrentPlane() {
+        return currentPlane;
+    }
+
+    @Override
+    public String toString() {
+        if (currentPlane != null) {
+            return "Runway " + id + " is occupied by flight " + currentPlane.getFlightCode();
+        } else {
+            return "Runway " + id + " is available";
+        }
+    }
+}
