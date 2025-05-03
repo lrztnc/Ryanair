ea22ea40ea2fca0ad2756a785e4e66972bbe99df
lrztnc <lorenzo.tincani@icloud.com>
2025-05-03 17:33:41 +0200

    Implement ParkingArea class to manage plane parking operations

---
+++ ParkingArea.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParkingArea {
+    private final int maxCapacity = 3; 
+    private List<Plane> parkedPlanes;
+
+    public ParkingArea() {
+        this.parkedPlanes = new ArrayList<>();
+    }
+
+  
+    public boolean parkPlane(Plane plane) {
+        if (parkedPlanes.size() < maxCapacity) {
+            parkedPlanes.add(plane);
+            System.out.println("Plane " + plane.getCode() + " parked successfully.");
+            return true;
+        } else {
+            System.out.println("Parking area is full. Plane " + plane.getCode() + " cannot be parked.");
+            return false;
+        }
+    }
+
+   
+    public boolean removePlane(Plane plane) {
+        if (parkedPlanes.contains(plane)) {
+            parkedPlanes.remove(plane);
+            System.out.println("Plane " + plane.getCode() + " removed from parking area.");
+            return true;
+        } else {
+            System.out.println("Plane " + plane.getCode() + " is not in the parking area.");
+            return false;
+        }
+    }
+
+    
+    public int getAvailableSpots() {
+        return maxCapacity - parkedPlanes.size();
+    }
+
+    public boolean isFull() {
+        return parkedPlanes.size() >= maxCapacity;
+    }
+   
+    public List<Plane> getParkedPlanes() {
+        return new ArrayList<>(parkedPlanes);
+    }
+}
